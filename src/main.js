bd515a988d08272fdf14d1fd0616e8342fa872cb
allisonylee <88457470+allisonylee@users.noreply.github.com>
2026-02-09 16:49:48 -0500

    todos-03: Configure Prettier and GitHub Pages deployment

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import "./style.css";
 
 const app = document.getElementById("app");
-app.innerHTML =  `<div class="flex items-center justify-center h-screen">Hello World!</div>`;
+app.innerHTML = `<div class="flex items-center justify-center h-screen">Hello World!</div>`;
