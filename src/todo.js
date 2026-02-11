cd35a3564d69cb4997416c857555c621a28a75f0
allisonylee <88457470+allisonylee@users.noreply.github.com>
2026-02-11 16:42:14 -0500

    todos-15: Create Todo class with basic methods

---
+++ src/todo.js
@@ -0,0 +1,16 @@
+class Todo {
+    constructor(id, text, completed = false) {
+        this.id = id;
+        this.text = text;
+        this.completed = completed;
+    }
+
+    toggle() {
+        this.completed = !this.completed;
+    }
+}
+
+const todo = new Todo(1, "Buy milk");
+console.log(todo);
+todo.toggle();
+console.log(todo);
